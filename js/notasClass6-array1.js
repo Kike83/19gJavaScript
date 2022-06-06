185639059735278263643233e92f4227d2a6cb27
KikeBA <enrique.berdichevsky@gmail.com>
2022-06-05 22:53:35 -0500

    ADD class 6

---
+++ js/notasClass6-array1.js
@@ -0,0 +1,65 @@
+
+
+// objetc --> array
+
+
+// Tipos de datos --> dentro de un Array
+// let newArray = []
+// let newArray = [1,2,4]
+// let newArray = ['str', 'str2']
+// let newArray = [ [9,2,3], [1,2] ]
+// let newArray = [ { a:2 } ]
+
+
+// Aquí vemos:
+
+// 1.- Enunciar un Array
+
+// let newArray = [1,2,3, 'str',[1,2] ]
+
+// console.log(newArray)
+// .length --> tamaño del array --> cantidad de elementos que contie
+
+
+// 2.- acceder a una posición específica de un Array
+
+// console.log(newArray[3])
+// console.log(newArray[4][1])
+
+
+// 3.- como iterar un Array
+
+// for ( i = 0 ; i < newArray.length; i++) {
+//     console.log(newArray[i])
+//     //console.log(i)
+// }
+
+let arrayToReverse = [1,2,3]
+// --> [1,2,3] --> [3,2,1]
+let arrReversed = []
+
+for( i = arrayToReverse.length -1; i >= 0; i-- ) {
+    arrReversed.push( arrayToReverse[i])
+}
+
+console.log(arrayToReverse, arrReversed)
+arrReversed.push[3]
+arrReversed.push[3]
+arrReversed[0] = 10
+console.log(arrReversed)
+
+arrReversed[5] = 100
+console.log(arrReversed)
+
+arrReversed[ arrReversed.length ] = 1000
+console.log(arrReversed)
+
+// Metodos de array
+
+// arr.push(item)
+// .push() --> agregar nuevo item al array
+// .pop() --> agregar nuevo item al array
+
+
+
+
