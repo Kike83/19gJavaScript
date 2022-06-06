cbacb139230d379f69cc4df386d3cb431008fd2e
KikeBA <enrique.berdichevsky@gmail.com>
2022-06-05 23:47:20 -0500

    ADD class Community 1

---
+++ js/notasClass4.2-community1-Jorge.js
@@ -0,0 +1,41 @@
+
+
+// Repaso de los 1eros 3 grandes temas hasta, hoy:
+
+
+/*
+ * Condicionales
+ * --> Operadores de asignación
+ * --> Operadores de comparación
+ * 
+ * */ 
+ 
+
+
+// 2 escenarios
+let age = 17
+if(age >= 18){
+
+}
+
+
+if(condicion){
+
+} else {
+
+}
+
+||
+
+
+condición ? true 
+    : // esto es como el ELSE 
+    flase
+
+
+/*
+Metodos de string
+*/
+
+
+
