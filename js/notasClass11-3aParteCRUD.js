1d0aad6ce741f86d0abd4385a7e4c3c6f3150fe5
KikeBA <enrique.berdichevsky@gmail.com>
2022-06-05 23:19:24 -0500

    ADD class 11

---
+++ js/notasClass11-3aParteCRUD.js
@@ -0,0 +1,47 @@
+
+
+// empezó tema nuevo
+// 10.01 pm
+// 3a gran parte de Hoy
+
+
+
+
+// 4 acciones sobre un objeto
+
+// CRUD
+
+// C --> Create
+// R --> Read
+// U --> Update
+// D --> Delete
+
+
+
+// C --> Create
+
+let objectCar = {
+    model: 'jetta',
+    doors: 4,
+    year: 2022,
+    cilindraje: 2,
+    color: 'rojo'
+    }
+
+
+
+// R --> Read
+
+console.log(objectCar)
+
+
+
+
+// U --> Update
+objectCar.color = 'verde'
+
+
+
+// D --> Delete
+delete objectCar.cilindraje
+
