438e6617f759804a2bf5f01111a3b2961a5864d6
KikeBA <enrique.berdichevsky@gmail.com>
2022-06-09 02:50:46 -0500

    ADD class 16

---
+++ js/notasClass16.3-bom.js
@@ -0,0 +1,76 @@
+
+
+// 9:10 pm regresamos de break
+
+// 2 min, Rama de hoy --> class-16
+
+
+
+// Tema de hoy: BOM
+
+
+// ya vimos:
+// DOM
+// Document Object Model
+// manipular el documento html
+// document.querySelector('item')
+// element.classlist.add('clasenueva)
+// element.innerHTML = 'nuevohtml
+// element.addAttribute('propiedad', 'valor')
+// element.getAttribute('rpopiedad')
+// document.getElementsByTagName('h2')[1]
+// document.createElement('h3')
+// document.getElementById('iddelinput').value
+
+// element.addEventListener('click', 'callback')
+// callback = la referencia a una función
+// element.addEventListener('click', () => { ...} )
+
+// cómo manipular el html
+// cómo agregar y escuchar eventos
+// cómo mover elementos
+
+
+
+
+
+// hoy:
+// BOM
+// Browse Object Model
+// comunicarse con el navegador
+
+
+// window
+// window.innerWidth;
+// window.innerHeigh;
+
+
+// 9:50 pm
+// window.screen
+
+// window.navigator
+
+// window.history.back()
+// window.history.forward()
+
+// 10.11 pm
+// window.location
+
+// location.reload()
+// location.search()
+
+
+
+// Tarea: mañana
+// AJAX
+// qué es?
+// Diferentes formas de implementarlo
+// hay 3, las 2 principales
+
+// hasta aquí, 10.16 pm, clase dde hoy
+
+
+
+
+
+
